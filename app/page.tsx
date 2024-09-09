ae8dad2d8c6d610cab8f0b6f6a0defb396756205
digitag62 <144676268+digitag62@users.noreply.github.com>
2024-09-09 07:34:47 +0000

    base

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="container m-auto border">
+      <div className="flex justify-center py-10">
+        <h2 className="">Hello John</h2>
+      </div>
+    </div>
+  );
+}
